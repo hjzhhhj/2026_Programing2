5be62e6a56e85099e9a0220a9a8e9f2656c0abf6
hjzhhhj <jhj090120@gmail.com>
2026-04-09 09:08:28 +0900

    TodoListApp HTML className, todolist.css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,33 +1,30 @@
+import "./todolist.css";
+
 function TodoListApp() {
   return (
-    <div>
-      <h1>No Think, Do It!</h1>
-      <form>
-        <input type="text" placeholder="할 일을 입력하세요" />
-        <button>Add</button>
+    <div className="todo">
+      <h1 className="todo__title">No Think, Do It.</h1>
+      <form className="todo__form">
+        <input
+          type="text"
+          placeholder="할 일을 입력하세요."
+          className="todo__input"
+        />
+        <button type="submit" className="todo__button todo__button--add">
+          Add
+        </button>
       </form>
-
-      <ul>
-        <li>
-          <label htmlFor="chk-1">
-            <input type="checkbox" name="" id="chk-1" /> 할 일 1
-          </label>
-          <button>❌</button>
-          <button>✏️</button>
+      <ul className="todo__list">
+        <li className="todo__item todo__item--empty">
+          <p>할 일 없음.</p>
         </li>
-        <li>
-          <label htmlFor="chk-2">
-            <input type="checkbox" name="" id="chk-2" /> 할 일 2
+        <li className="todo__item todo__item--complete">
+          <input type="checkbox" id="chk-1" className="todo__check" />
+          <label htmlFor="chk-1" className="todo__label">
+            think
           </label>
-          <button>❌</button>
-          <button>✏️</button>
-        </li>
-        <li>
-          <label htmlFor="chk-3">
-            <input type="checkbox" name="" id="chk-3" /> 할 일 3
-          </label>
-          <button>❌</button>
-          <button>✏️</button>
+          <button className="todo__button todo__button--edit">✏️</button>
+          <button className="todo__button todo__button--delete">❌</button>
         </li>
       </ul>
     </div>
