edcef7a4354f557d92fa0a3ba82f56859a1ea553
hjzhhhj <jhj090120@gmail.com>
2026-04-17 09:35:06 +0900

    TodoListApp <TodoItem key={} />

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,7 +7,8 @@ export default function TodoList({ todos }) {
       {/* todo에 값이 없으면, TodoItemEmpty  */}
       {todos.length === 0 && <TodoItemEmpty />}
       {/* todos에 값이 있으면, TodoItem에 todo에 넣자 */}
-      {todos.length > 0 && todos.map((todo) => <TodoItems todo={todo} />)}
+      {todos.length > 0 &&
+        todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)}
     </ul>
   );
 }
